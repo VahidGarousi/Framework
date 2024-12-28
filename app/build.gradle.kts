cd2e49a245791c668474692af47922aaa77e3951
Vahid Garousi <garousiv@gmail.com>
2024-12-28 12:41:41 +0330

    Update build.gradle.kts and gradle.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ir.vahid.framework"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ir.vahid.framework"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
