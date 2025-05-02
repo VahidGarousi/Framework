e2e263e356859ca32ab6493d88ba7dc1309c4c28
Vahid Garousi <garousiv@gmail.com>
2025-05-02 12:01:45 +0330

    #123 Fix `testDevelopDebug` issue

--- build-logic/convention/src/main/kotlin/AndroidApplicationJacocoConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationJacocoConventionPlugin.kt
@@ -4,11 +4,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
 import ir.vahid.framework.configureJacoco
+import org.gradle.kotlin.dsl.apply
 
 class AndroidApplicationJacocoConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("jacoco")
+            apply(plugin = "jacoco")
             val androidExtension = extensions.getByType<ApplicationExtension>()
 
             androidExtension.buildTypes.configureEach {
